cbedd9a391d048a993476cd9ade1f0e2f0255619
Emmanuel Podestá Jr <epodesta158@gmail.com>
2020-11-12 13:46:08 -0300

    Changed workset in FN app.

--- x86/src/FN/fn.h
+++ x86/src/FN/fn.h
@@ -18,7 +18,7 @@ extern void friendly_numbers();
  * Parameters                                                                 *
  *============================================================================*/
 
-#define PROBLEM_SIZE                                 11605
+#define PROBLEM_SIZE                                 17067
 #define PROBLEM_START_NUM                          1000001
 #define PROBLEM_END_NUM      (PROBLEM_START + PROBLEM_SIZE)
 
