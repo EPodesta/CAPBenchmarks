778ce52cb7453798a4dead3da61d1ff8f6ec1b0b
Emmanuel Podestá Junior <epodesta@pm.me>
2020-11-18 19:27:07 -0300

    Removed pragmas from LU.

--- x86/src/LU/lower_upper.c
+++ x86/src/LU/lower_upper.c
@@ -40,7 +40,6 @@ static void _swap_rows(struct matrix *m, int i1, int i2)
 	float tmp; /* Temporary value. */
 
 	/* Swap columns. */
-	#pragma omp parallel for private(j, tmp) default(shared)
 	for (j = 0; j < m->width; j++)
 	{
 		tmp = MATRIX(m, i1, j);
@@ -58,7 +57,6 @@ static void _swap_columns(struct matrix *m, int j1, int j2)
 	float tmp; /* Temporary value. */
 
 	/* Swap columns. */
-	#pragma omp parallel for private(i, tmp) default(shared)
 	for (i = 0; i < m->height; i++)
 	{
 		tmp = MATRIX(m, i, j1);
@@ -79,35 +77,28 @@ static float _find_pivot(struct matrix *m, int i0, int j0)
 	ipvt = i0;
 	jpvt = j0;
 
-	#pragma omp parallel private(i, j, pipvt, pjpvt) shared(m, i0, j0)
+	pipvt = i0;
+	pjpvt = j0;
+
+	/* Find pivot element. */
+	for (i = i0; i < m->height; i++)
 	{
-		pipvt = i0;
-		pjpvt = j0;
-
-		/* Find pivot element. */
-		#pragma omp for
-		for (i = i0; i < m->height; i++)
+		for (j = j0; j < m->width; j++)
 		{
-			for (j = j0; j < m->width; j++)
+			/* Found. */
+			if (fabs(MATRIX(m, i, j)) < fabs(MATRIX(m,pipvt,pjpvt)))
 			{
-				/* Found. */
-				if (fabs(MATRIX(m, i, j)) < fabs(MATRIX(m,pipvt,pjpvt)))
-				{
-					pipvt = i;
-					pjpvt = j;
-				}
+				pipvt = i;
+				pjpvt = j;
 			}
 		}
+	}
 
-		/* Reduct. */
-		#pragma omp critical
-		{
-			if (fabs(MATRIX(m, pipvt, pjpvt)) > fabs(MATRIX(m, ipvt, jpvt)))
-			{
-				ipvt = pipvt;
-				jpvt = pjpvt;
-			}
-		}
+	/* Reduct. */
+	if (fabs(MATRIX(m, pipvt, pjpvt)) > fabs(MATRIX(m, ipvt, jpvt)))
+	{
+		ipvt = pipvt;
+		jpvt = pjpvt;
 	}
 
 	_swap_rows(m, i0, ipvt);
@@ -128,7 +119,6 @@ static void _row_reduction(struct matrix *m, int i0, float pivot)
 	j0 = i0;
 
 	/* Apply row redution in some lines. */
-	#pragma omp parallel for private(i, j, mult) default(shared)
 	for (i = i0 + 1; i < m->height; i++)
 	{
 		mult = MATRIX(m, i, j0)/pivot;
@@ -166,7 +156,6 @@ int lower_upper(struct matrix *m, struct matrix *l, struct matrix *u)
 	}
 
 	/* Build upper and lower matrixes.  */
-	#pragma omp parallel for private(i, j) default(shared)
 	for (i = 0; i < m->height; i++)
 	{
 		for (j = 0; j < m->width; j++)
